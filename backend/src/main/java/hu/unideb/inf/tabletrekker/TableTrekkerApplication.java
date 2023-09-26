e722b8cbc361ad41f039295d1e456099c17d40d2
Szél Szabolcs Bálint <szel.szabolcs02@gmail.com>
2023-09-26 20:31:22 +0200

    First backend commit

---
+++ backend/src/main/java/hu/unideb/inf/tabletrekker/TableTrekkerApplication.java
@@ -0,0 +1,13 @@
+package hu.unideb.inf.tabletrekker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TableTrekkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TableTrekkerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/hu/unideb/inf/tabletrekker/TableTrekkerApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.unideb.inf.tabletrekker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TableTrekkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
