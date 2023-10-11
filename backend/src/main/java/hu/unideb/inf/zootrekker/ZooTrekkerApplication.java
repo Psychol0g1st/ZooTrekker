aae9433f8524b1b9de9da0f9d5c1a265dc34aaea
Szél Szabolcs Bálint <szel.szabolcs02@gmail.com>
2023-10-11 11:20:13 +0200

    Renaming main class

--- backend/src/main/java/hu/unideb/inf/zootrekker/ZooTrekkerApplication.java
+++ backend/src/main/java/hu/unideb/inf/zootrekker/ZooTrekkerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TableTrekkerApplication {
+public class ZooTrekkerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TableTrekkerApplication.class, args);
+		SpringApplication.run(ZooTrekkerApplication.class, args);
 	}
 
 }
