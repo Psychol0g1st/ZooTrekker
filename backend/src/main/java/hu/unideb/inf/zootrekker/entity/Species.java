318876aa4ebb3dd4ad3537b13e4df22a19c861ae
szilagyiabel <144916591+szilagyiabel@users.noreply.github.com>
2023-10-17 20:58:23 +0200

    Timestamp2

--- backend/src/main/java/hu/unideb/inf/zootrekker/entity/Species.java
+++ backend/src/main/java/hu/unideb/inf/zootrekker/entity/Species.java
@@ -5,7 +5,11 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
+
+import java.sql.Timestamp;
 import java.util.List;
 
 @NoArgsConstructor
@@ -19,4 +23,13 @@ public class Species {
     private Long id;
     @Column(unique = true)
     private String name;
+
+    @CreationTimestamp
+    @Column(name="created_at", updatable = false, nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private Timestamp createdAt;
+    @Column(name="updated_at")
+    @UpdateTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    private Timestamp updatedAt;
 }
