672ed8bee886379f0ca96ce36589b40110a0b7e4
Svec Antal <anton.svec13@gmail.com>
2023-10-31 10:57:43 +0100

    Create test environmnet

--- backend/src/main/java/hu/unideb/inf/zootrekker/ZooTrekkerApplication.java
+++ backend/src/main/java/hu/unideb/inf/zootrekker/ZooTrekkerApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ZooTrekkerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ZooTrekkerApplication.class, args);
 	}
