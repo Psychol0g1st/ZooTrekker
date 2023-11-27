b26344ba3e013aaa6d38af6b4ea498433ae38672
harangozomate <harangozmate18@gmail.com>
2023-11-27 11:29:56 +0100

    Bug fixes

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -16,9 +16,7 @@ export default function Navbar() {
 
                 </div>
 
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
+                
             </div>
         </nav>
         </div>
